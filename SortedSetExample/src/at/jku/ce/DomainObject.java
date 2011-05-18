a8043b499662b38d9b6a5c2226f9103c81e78c3c
nikoniko <n.spanos@gmx.at>
2011-05-18 13:44:28 +0200

    Tag 1: Arthur: id und konstruktur hinzugefügt

--- SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -2,6 +2,14 @@ package at.jku.ce;
 
 public class DomainObject {
 
+
+	public String id;
+
+	public DomainObject(String id){
+		super();
+		this.id = id;
+	}
+
 	public DomainObject() {
 		super();
 	}
