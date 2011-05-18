3036acc6aa4287e6f74ce745a3d9e8839d3f685c
Iffi01 <iffi_der_held@yahoo.de>
2011-05-18 13:47:22 +0200

    <Tag 1> Clemens definiert einen Logger

--- SortedSetExample/src/at/jku/ce/Main.java
+++ SortedSetExample/src/at/jku/ce/Main.java
@@ -2,10 +2,24 @@ package at.jku.ce;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+	Set<DomainObject> objectSet = new HashSet<DomainObject>();
+
+
 	public static void main(String[] args) {
 	}
 
+
+	objectSet.add(new DomainObject("Helga")); 
+	objectSet.add(new DomainObject("Walter")); 
+	objectSet.add(new DomainObject("Hannah")); 
+	objectSet.add(new DomainObject("Paul")); 
+	objectSet.add(new DomainObject("Claudia"));
+
+	for (DomainObject domainObject : objectSet) { 
+		LOGGER.debug(domainObject.id);
+
+	}
 }
