6d1c5b6cf8ff5db27f47a21e4f71f31950bcbbdb
nikoniko <n.spanos@gmx.at>
2011-05-18 14:11:16 +0200

    Tag 4: Arthur: comment hinzugefügt

--- SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -5,6 +5,7 @@ public class DomainObject {
 
 	private String uuid = UUID.randomUUID().toString();
 	private String name;
+	private String comment;
 
 	public DomainObject(String id){
 		super();
@@ -21,5 +22,13 @@ public class DomainObject {
 	public String getUuid(){
 		return uuid;
 	}
+
+	public String getComment(){
+		return comment;
+	}
+	public void setComment(String commend){
+		this.comment = comment;
+	}
+
 
 }
