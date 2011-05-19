b563b546df746d30e4048e1ff4311ebba8a2ae4a
Iffi01 <iffi_der_held@yahoo.de>
2011-05-19 19:34:49 +0200

    <Tag 11> protected

--- SortedSetExample/src/at/jku/ce/DomainObject.java
+++ SortedSetExample/src/at/jku/ce/DomainObject.java
@@ -27,24 +27,18 @@ public class DomainObject {
 	protected String getComment(){
 		return comment;
 	}
-<<<<<<< HEAD
+
 	protected void setComment(final String commend){
-=======
-	public void setComment(final String commend){
->>>>>>> 26bdd2371b7982bfe680fc1108b5f79b2a67ceb9
+
 		this.comment = comment;
 	}
 
 	protected String getName(){
 		return name;
 	}
-<<<<<<< HEAD
+
 	protected void setName(final String name){
-=======
-	public void setName(final String name){
->>>>>>> 26bdd2371b7982bfe680fc1108b5f79b2a67ceb9
 		this.name = name;
 	}
-
 
 }
